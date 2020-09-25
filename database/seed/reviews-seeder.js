72e62b4050c597c6e0079c1b175941377b496c6b
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-09-25 15:05:19 -0700

    Implemented AWS S3 to seeding script

--- database/seed/reviews-seeder.js
+++ database/seed/reviews-seeder.js
@@ -16,7 +16,7 @@ for (let i = 1; i <= 100; i++) {
     data.user_data.push(
       {
         username: faker.name.firstName(),
-        image: 'https://loremflickr.com/320/240/dog',
+        image: `https://ghrsea12-fec.s3-us-west-2.amazonaws.com/sample/pic${i}.jpg`,
         review: faker.lorem.sentence(),
         cleanliness: Math.floor(Math.random() * 5) + 1,
         communication: Math.floor(Math.random() * 5) + 1,
