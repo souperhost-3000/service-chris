fc5adce2aebbd9f8c0d42096a735e8b40afa25db
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-09-25 15:02:56 -0700

    Update

--- database/seed/reviews-seeder.js
+++ database/seed/reviews-seeder.js
@@ -16,7 +16,7 @@ for (let i = 1; i <= 100; i++) {
     data.user_data.push(
       {
         username: faker.name.firstName(),
-        image: 'https://ghrsea12-fec.s3-us-west-2.amazonaws.com/sample/pic1.jpg',
+        image: 'https://loremflickr.com/320/240/dog',
         review: faker.lorem.sentence(),
         cleanliness: Math.floor(Math.random() * 5) + 1,
         communication: Math.floor(Math.random() * 5) + 1,
