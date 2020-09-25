cadcbd6f1821cee417e6226711a62e2988719755
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-09-25 14:58:39 -0700

    Update

--- database/seed/reviews-seeder.js
+++ database/seed/reviews-seeder.js
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
-const db = require('../index.js');
-const User = require('../models/user.js');
+require('../index.js');
+const aws = require('aws-sdk');
+const { User } = require('../models/user.js');
 const mongoose = require('mongoose');
 const faker = require('faker');
 
@@ -15,7 +16,7 @@ for (let i = 1; i <= 100; i++) {
     data.user_data.push(
       {
         username: faker.name.firstName(),
-        image: 'https://loremflickr.com/320/240/dog',
+        image: 'https://ghrsea12-fec.s3-us-west-2.amazonaws.com/sample/pic1.jpg',
         review: faker.lorem.sentence(),
         cleanliness: Math.floor(Math.random() * 5) + 1,
         communication: Math.floor(Math.random() * 5) + 1,
