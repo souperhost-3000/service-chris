f614efa853d3f2eeb440f85d6b01aa5c7f2bb58b
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-09-25 19:11:43 -0700

    Changed randomness of image

--- database/seed/reviews-seeder.js
+++ database/seed/reviews-seeder.js
@@ -1,6 +1,5 @@
 /* eslint-disable camelcase */
 require('../index.js');
-const aws = require('aws-sdk');
 const { User } = require('../models/user.js');
 const mongoose = require('mongoose');
 const faker = require('faker');
@@ -13,10 +12,11 @@ for (let i = 1; i <= 100; i++) {
   };
 
   for (let j = 0; j < Math.floor(Math.random() * 30) + 1; j++) {
+    let ran = Math.floor(Math.random() * 100) + 1;
     data.user_data.push(
       {
         username: faker.name.firstName(),
-        image: `https://ghrsea12-fec.s3-us-west-2.amazonaws.com/sample/pic${i}.jpg`,
+        image: `https://ghrsea12-fec.s3-us-west-2.amazonaws.com/sample/pic${ran}.jpg`,
         review: faker.lorem.sentence(),
         cleanliness: Math.floor(Math.random() * 5) + 1,
         communication: Math.floor(Math.random() * 5) + 1,
